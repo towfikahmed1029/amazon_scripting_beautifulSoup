05e80fa1aba6b1ec6d158c503c8beaf6f280a913
Towfik Ahmed <75902572+towfikahmed1029@users.noreply.github.com>
2023-01-10 00:56:08 +0600

    Update amz_url.py

--- amz_url.py
+++ amz_url.py
@@ -5,11 +5,11 @@ import sys
 import os
 if sys.platform in ['Windows', 'win32', 'cygwin']:
     mydb = mysql.connector.connect(
-    host = "database-1.criq1nathhcp.us-west-2.rds.amazonaws.com",
-    user = "admin",
-    password = "5HjTd1Fr7e3DS",
-    database = "affiliate",
-    port=33360
+    host = "",
+    user = "",
+    password = "",
+    database = "",
+    port=
     )
 else:
     mydb = mysql.connector.connect(
